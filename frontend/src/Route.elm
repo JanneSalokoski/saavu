dbfaf9a336606667b77dc714d89ab12e40c7b217
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-06-02 10:41:19 +0300

    Add Route.elm
    Route.elm is responsible for parsing the routes of our application

---
+++ frontend/src/Route.elm
@@ -0,0 +1,28 @@
+module Route exposing (..)
+
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>))
+
+
+type Route
+    = Home
+    | Admin
+    | Event String
+    | CreateEvent
+    | NotFound
+
+
+routeParser : Parser.Parser (Route -> Route) Route
+routeParser =
+    Parser.oneOf
+        [ Parser.map Home Parser.top
+        , Parser.map Admin (Parser.s "admin")
+        , Parser.map Event (Parser.s "event" </> Parser.string)
+        , Parser.map CreateEvent (Parser.s "event")
+        ]
+
+
+fromUrl : Url -> Route
+fromUrl url =
+    Parser.parse routeParser url
+        |> Maybe.withDefault NotFound
